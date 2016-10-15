211b76145e3a22402432a6e6f541b4733016064d
Lauren Beatty <lauren.m.beatty@gmail.com>
2016-10-15 18:52:09 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $(".hamburger").on("click", function() {
+
+        if ($(".slidedown-menu").is(":visible")) {
+            $(this).toggleClass("is-active");
+            $(".slidedown-menu").fadeOut();
+            $(".site-header").animate({
+                height: "0px"
+            }, {
+                duration: 500,
+                queue: false
+            });
+        } else {
+            $(this).toggleClass("is-active");
+            $(".site-header").animate({
+                height: "1000px"
+            }, {
+                duration: 500,
+                queue: false
+            });
+
+            $(".slidedown-menu").fadeIn();
+        }
+    });
+
+    $("#typed").typed({
+        stringsElement: $('#typed-strings')
+    });
+
+
+});
